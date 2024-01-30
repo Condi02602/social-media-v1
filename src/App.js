309bb1e2d62bf23dcbc414516f77f7dd79ad284d
Conrad <conradaruldas2602@gmail.com>
2024-01-30 14:18:52 +0530

    Initialize project using Create React App Commited to main branch

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Sidebar from "./components/Sidebar/Sidebar";
+import CreatePost from "./components/CreatePost/CreatePost";
+import PostList from "./components/PostList/PostList";
+import {useState} from 'react';
+import PostListProvider from "./store/post-list-store";
 
 function App() {
+
+  const [selectedTab, setSelectedTab]  = useState("Home");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <PostListProvider>
+      <div className="app-container">
+        <Sidebar selectedTab={selectedTab} setSelectedTab={setSelectedTab}></Sidebar>
+        <div className="content">
+          <Header></Header>
+          {selectedTab === 'Home' ? (<PostList/>) : (<CreatePost/>)}
+          
+          <Footer></Footer>
+        </div>
+      </div>
+    </PostListProvider>
+    </>
   );
 }
 
